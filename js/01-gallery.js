09c7183568479a149bc101e6fe2f4570658fbe7d
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-01-22 13:10:11 +0200

    Add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkup(images) { 
+    return images.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join(''); 
+}
+
+function onGalleryContainerClick(event) { 
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) { 
+        return;
+    }
+    const urlLargeImg = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${urlLargeImg}" width="800" height="600">`)
+    instance.show();
+    if (instance) { 
+        document.addEventListener("keydown", event => {
+            if (event.code === 'Escape') { 
+                instance.close();
+            }
+        });
+    }
+}
