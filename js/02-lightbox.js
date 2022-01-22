772905fa1b4a73a03337f6d83e0165500c860780
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-01-22 15:19:50 +0200

    Add task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    'captionsData': 'alt',
+    'captionDelay': 250,
+});
+console.log(lightbox);
+
+
+function createGalleryMarkup(images) { 
+    return images.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>
+        `
+    }).join(''); 
+}
+
